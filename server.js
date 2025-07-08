b53907970c9d73171fa7dcf4304878384d322e83
HaiPham702 <phamhainthp@gmail.com>
2025-07-08 22:53:44 +0700

    Update: Modify CORS middleware in server.js to include 'x-auth-token' in allowed headers

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(cors({
     origin: '*',
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
+    allowedHeaders: 'Content-Type, Authorization, x-auth-token',
 }));
 app.use(express.json());
 
